bc69574cc25de8191778b791d0c066c570c037e0
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-22 18:40:52 +0000

    search bar on the sidebar

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import "./Sidebar.css"
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { IconButton, Avatar } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>sidebar</h1>
+            <div className="sidebar__header">
+                <Avatar src="https://avatars.githubusercontent.com/u/62023426?s=460&u=91fcda8d6cb8abba9aeeb1463badefae0a508fee&v=4"/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or Start new chat" type="text"/>
+                </div>
+            </div>
+
         </div>
     )
 }
