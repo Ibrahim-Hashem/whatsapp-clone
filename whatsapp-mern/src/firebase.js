d0fcf84cde854a526580bbc811afc3aa268c593b
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-22 17:51:12 +0000

    added the app sections

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDosO8bO6E3NV0hxHNVyYQIMDJPCmwaFmI",
+    authDomain: "whatsapp-mern-6f461.firebaseapp.com",
+    projectId: "whatsapp-mern-6f461",
+    storageBucket: "whatsapp-mern-6f461.appspot.com",
+    messagingSenderId: "116141633845",
+    appId: "1:116141633845:web:f7996c3766dafba2779c7c"
+  };
