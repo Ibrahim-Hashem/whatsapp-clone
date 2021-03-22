067adaf1431f6b57479b76a488afe1df5016bb27
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-22 19:45:35 +0000

    this is the sidebar chat components

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -5,6 +5,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import { IconButton, Avatar } from '@material-ui/core';
 import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
@@ -26,8 +27,13 @@ function Sidebar() {
             <div className="sidebar__search">
                 <div className="sidebar__searchContainer">
                     <SearchOutlined/>
-                    <input placeholder="Search or Start new chat" type="text"/>
+                    <input placeholder="Search" type="text"/>
                 </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
 
         </div>
