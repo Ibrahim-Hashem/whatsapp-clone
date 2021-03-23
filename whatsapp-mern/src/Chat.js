859af06160e660991fa8c77fafa6c5cf8e1b53bb
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-23 15:27:49 +0000

    mern

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,5 +1,6 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@material-ui/icons'
+import MicIcon from '@material-ui/icons/Mic'
 import React from 'react'
 import "./Chat.css"
 
@@ -26,12 +27,25 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p>
+                <p className="chat__message" >
                     <span className="chat__name"> Ibrahim</span>
                     This is a message
                     <span className="chat__timestamp">{new Date().toUTCString()}</span>
-
                 </p>
+                <p className="chat__message chat__reciever" >
+                    <span className="chat__name"> Ibrahim</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon/>
+                <form action="">
+                    <input type="text" placeholder="Type a message"/>
+                    <button type="submit" >Send a message </button>
+                </form>
+                <MicIcon/>
             </div>
 
         </div>
