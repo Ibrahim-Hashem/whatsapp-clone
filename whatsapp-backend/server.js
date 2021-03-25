41e8488502e4a3f6c543ac0daf2f7feffeaf8a77
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-25 17:17:53 +0000

    back end pusher

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,18 +1,26 @@
 //creating an api
-
-
 // import
 import express from 'express';
 import mongoose from 'mongoose'
-
+import Messages from './dbMessages.js';
+import Pusher from 'pusher'
 // app config
 const app = express() //creates application and will allow us to write api routes.
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9000;
+const pusher = new Pusher({
+    appId: "1177935",
+    key: "c21624dc31efdb828d12",
+    secret: "efba0736e53ba1d8a706",
+    cluster: "eu",
+    useTLS: true
+  });
 
 //middleware
+app.use(express.json())
 
 //DB config
-const connection_url = "mongodb+srv://admin:70tDBuvZtOaRNrnA@cluster0.vza9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const connection_url = "mongodb+srv://admin:70tDBuvZtOaRNrnA@cluster0.vza9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 
 mongoose.connect(connection_url, {
     useCreateIndex:true,
@@ -20,10 +28,48 @@ mongoose.connect(connection_url, {
     useUnifiedTopology:true
 })
 //????
-
+const db = mongoose.connection;
+db.once('open',()=>{
+    console.log('db connected');
+    const msgCollection = db.collection("messagecontents");
+    const changeStream = msgCollection.watch();
+    changeStream.on('change', change=>{
+        if(change.operationType ==='insert'){
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messages", "inserted", {
+                name:messageDetails.user,
+                message: messageDetails.message
+              });
+        }else{
+            console.log("Error triggering pusher")
+        }
+    });
+})
 //api route
 
 app.get('/',(req,res)=> res.status(200).send('hello world')) //send get request to app at the root url and it will return hello world if the status of the response is ok (200).
+app.get('/messages/sync', (req,res)=>{
+
+    Messages.find((err,data) =>{
+        if(err) {
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+app.post('/messages/new', (req,res)=>{
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err,data) =>{
+        if(err) {
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
+
 
 //listener
 
