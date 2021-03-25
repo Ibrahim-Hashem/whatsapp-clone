4755604412808c5b8a6428e734f45fd5c1f87ffe
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-25 23:55:58 +0000

    mern changes

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
+import React, { useEffect, useState } from 'react';
+import axios from "./axios";
+
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get('/messages/sync').then(response=>{
+      setMessages(response.data);
+      })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('c21624dc31efdb828d12', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind("inserted", function(data) {
+      alert(JSON.stringify(data));
+      setMessages([...messages,data]);
+    });
+  }, [])
+
+  console.log(messages);
+
+
   return (
     <div className="app">
       <div className="app__body">
